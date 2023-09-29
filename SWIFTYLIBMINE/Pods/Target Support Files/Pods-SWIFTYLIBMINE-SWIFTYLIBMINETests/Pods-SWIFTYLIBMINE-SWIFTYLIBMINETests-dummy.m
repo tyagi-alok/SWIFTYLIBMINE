b20e84e14125b08f9db71e4647bf7d45f525cec3
Alok Tyagi <alok.tyagi@magicbricks.com>
2023-09-29 22:35:44 +0530

    Added SwiftyLib.podspec

---
+++ SWIFTYLIBMINE/Pods/Target Support Files/Pods-SWIFTYLIBMINE-SWIFTYLIBMINETests/Pods-SWIFTYLIBMINE-SWIFTYLIBMINETests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SWIFTYLIBMINE_SWIFTYLIBMINETests : NSObject
+@end
+@implementation PodsDummy_Pods_SWIFTYLIBMINE_SWIFTYLIBMINETests
+@end
---
+++ SWIFTYLIBMINE/Pods/Target Support Files/Pods-SWIFTYLIBMINEExample/Pods-SWIFTYLIBMINEExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SWIFTYLIBMINEExample : NSObject
+@end
+@implementation PodsDummy_Pods_SWIFTYLIBMINEExample
+@end
