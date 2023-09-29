44c93f9d677a03a1e4d6ccddafcde415d66dd437
Alok Tyagi <alok.tyagi@magicbricks.com>
2023-09-29 15:04:17 +0530

    Initial Project

---
+++ SWIFTYLIBMINE/SWIFTYLIBMINE/SWIFTYLIBMINE.h
@@ -0,0 +1,18 @@
+//
+//  SWIFTYLIBMINE.h
+//  SWIFTYLIBMINE
+//
+//  Created by Alok Tyagi on 29/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SWIFTYLIBMINE.
+FOUNDATION_EXPORT double SWIFTYLIBMINEVersionNumber;
+
+//! Project version string for SWIFTYLIBMINE.
+FOUNDATION_EXPORT const unsigned char SWIFTYLIBMINEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWIFTYLIBMINE/PublicHeader.h>
+
+
