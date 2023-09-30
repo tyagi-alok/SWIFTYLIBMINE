8395ca7c7a1d988b132f99b9ebbfd0efe42785eb
Alok Tyagi <alok.tyagi@magicbricks.com>
2023-09-30 10:52:19 +0530

    new file added

---
+++ SWIFTYLIBMINE/SWIFTYLIBMINE/NetworkManager/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  SWIFTYLIBMINE
+//
+//  Created by Alok Tyagi on 30/09/23.
+//
+
+import Foundation
+
+public class NetworkManager{
+    
+    public init() {}
+    
+    public func getData(){
+        print("Getdata")
+    }
+}
